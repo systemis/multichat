244dbba8e1370efb7d6d9cb2a25b96bd3d592c26
systemis <systemofpeter@gmail.com>
2017-06-17 09:50:36 +0700

    setup router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import {BrowserRouter as Router, Route, hasHistory} from 'react-router-dom';
 import HomePage from './Page/Home/home.js';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {screenWidth: 0};
+  }
+
+  componentWillMount() {
+    this.setState({screenWidth: $(window).width()});
+  }
+
   render() {
+    const renderRouter = () => {
+      const width = this.state.screenWidth;
+      if(width > 768){
+        return <Route exact path='/' component={HomePage} />;
+      }else{
+        return (
+          <div>
+              <Route exact path='/' component={HomePage} />;
+          </div>
+        )
+      }
+    }
+    
     return (
       <Router>
         <div id="App">
-          <Route exact path='/' component={HomePage} />
+          {renderRouter()}
         </div>
       </Router>
     );
+  }
+
+  componentDidMount() {
+    window.onresize = () => {
+      this.setState({screenWidth: $(window).width()});
+    }    
   }
 }
 
