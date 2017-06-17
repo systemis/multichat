35ff54d68ab04aa079fd99d1f3ff9eafc40669e6
systemis <systemofpeter@gmail.com>
2017-06-17 09:16:46 +0700

    setup project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, hasHistory} from 'react-router-dom';
+import HomePage from './Page/Home/home.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={HomePage} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Page/Home/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className="home-page">
+                HomePage
+            </div>
+        );
+    }
+}
+
+export default HomePage;
