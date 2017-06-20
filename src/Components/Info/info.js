64b6528b23d5cf3a7e0c7452449bbfd84ce6f46b
systemis <systemofpeter@gmail.com>
2017-06-20 14:33:45 +0700

    info screen ui

--- src/Components/Info/info.js
+++ src/Components/Info/info.js
@@ -4,13 +4,29 @@ import './Style/info-group-style.css';
 
 class InfoGroup extends Component {
     render() {
+        // Class name to custom ui with desktop and mobile version .
         const className = () => {
             if(this.props.screenVersion === 'desktop'){
                 return 'desktop';
-            }
-        }
+            }}
         return (
             <div className={className()} id="info-group">
+                <div className="header-bar">
+                    <div className="dropdown">
+                        <span 
+                            className="show-client-name dropdown-toggle"
+                            data-toggle="dropdown">
+                            Matth Thomas
+                            <span className="caret" />
+                        </span>
+                        <ul className="dropdown-menu">
+                            <li className="dropdown-header">Dropdown header 1</li>
+                            <li><a href="#">HTML</a></li>
+                            <li><a href="#">CSS</a></li>
+                            <li><a href="#">JavaScript</a></li>
+                        </ul>
+                    </div>
+                </div>
                 {this.props.screenVersion}
             </div>
         );
