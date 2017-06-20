20e43acf0528f3e25f85afa1a0027716babfaf83
systemis <systemofpeter@gmail.com>
2017-06-20 16:12:43 +0700

    info screen ui done

--- src/Components/Info/info.js
+++ src/Components/Info/info.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect}            from 'react-redux';
+import testAvatar           from '../../Image/test_avatar_men.jpg';
 import './Style/info-group-style.css';
 
 class InfoGroup extends Component {
@@ -27,7 +28,43 @@ class InfoGroup extends Component {
                         </ul>
                     </div>
                 </div>
-                {this.props.screenVersion}
+                <div className="show-info">
+                    <div className="show-simple-info">
+                        <div className="show-avatar">
+                            <p>
+                                <img src={testAvatar} alt="User Avatar " />
+                            </p>
+                        </div>
+                        <div className="show-name-andress">
+                            <p className="show-name"   > Kristin </p>
+                            <p className="show-andress"> Cape </p>
+                        </div>
+                    </div>
+                    <div className="show-default-info">
+                        <table className="table-show-user-default-info">
+                            <tr> 
+                                <td className="show-title">Nick name:</td>
+                                <td className="show-value">Kili </td>
+                            </tr>
+                            <tr> 
+                                <td className="show-title">Tel:</td>
+                                <td className="show-value">072 143 9920</td>
+                            </tr>
+                            <tr> 
+                                <td className="show-title">Date of birth:</td>
+                                <td className="show-value">{new Date().toLocaleDateString()}</td>
+                            </tr>
+                            <tr> 
+                                <td className="show-title">Gender</td>
+                                <td className="show-value">Male</td>
+                            </tr>
+                            <tr> 
+                                <td className="show-title">Language</td>
+                                <td className="show-value">English</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
         );
     }
