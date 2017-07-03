9394f40a93ffdffe6f876d5618b67b0523ec1474
systemis <cuthinh2002@icloud.com>
2017-07-03 14:44:13 +0700

    Change send message struct

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 module.exports = mysql.createConnection({
     connectionLimit: 100,
-    host: 'localhost',
-    port: 8889,
-    user: 'root',
-    password: 'root',
-    database: 'ChatTogether',
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    host: '',
+    port: 3306,
+    user: '',
+    password: '',
+    database: '',
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
