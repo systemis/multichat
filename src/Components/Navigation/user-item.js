4c89bd9d630db77ec63f40786872a231b5553fc1
systemis <cuthinh2002@icloud.com>
2017-07-04 22:02:24 +0700

    fix bug when scale image show user avatar

--- src/Components/Navigation/user-item.js
+++ src/Components/Navigation/user-item.js
@@ -145,8 +145,9 @@ class UserItem extends Component {
     }
 
     componentDidMount() {
-        
-    }
+        const width = $(`#${this.state.itemClassName} > img`).css('width');
+        $(`#${this.state.itemClassName} > img`).css({'height': width});
+    }   
     
     shouldComponentUpdate(nextProps, nextState) {
         return true;
