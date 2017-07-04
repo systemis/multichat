4996c92ef55acf84544f300d58d8980e4ec7d158
systemis <cuthinh2002@icloud.com>
2017-07-04 19:09:23 +0700

    hiden hiden

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 module.exports = mysql.createConnection({
     connectionLimit: 100,
-    host: 'sql12.freemysqlhosting.net',
+    host: '',
     port: 3306,
-    user: 'sql12182975',
-    password: '1fSE1grs3g',
-    database: 'sql12182975',
+    user: '',
+    password: '',
+    database: '',
     // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
